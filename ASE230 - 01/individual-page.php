5c36de806937ca8b7b7cf5871814c806721e6122
randomusername01 <32079857+randomusername01@users.noreply.github.com>
2023-08-28 18:21:30 -0400

    Add files via upload
    Converted to php

---
+++ ASE230 - 01/individual-page.php
@@ -0,0 +1,242 @@
+
+<!DOCTYPE html>
+
+<html lang="en"> 
+<head>
+    <?php
+    $title = "Jacob Nelson's Resume";
+    $author = "Jacob Nelson";
+    $fav = "favicon.ico";
+    $job_title = "Merchandiser";
+    $description = "I am a student at NKU studying Applied Software Engineering";
+    $email = "nelsonj18@nku.edu";
+    $job_desc = "Merchandising for vendors, executing projects assigned by headquarters, and driving heavy equipment";
+    $degree = "Applied Software Engineering";
+    $college = "Northern Kentucky University";
+    $hobby1 = "Reverse Engineering";
+    $hobby2 = "Gaming";
+    $hobby3 = "Keyboards";
+    $linkedin = "linkedin.com/in/jacobnelson367";
+    $github = "github.com/randomusername01";
+    $workplace = "Home Depot";
+    $workdate = "2022 - Present";
+    $proj1name = "VrooomGG Website and DMG Calc";
+    $proj2name = "Maplestory Reverse Engineered";
+    $proj1desc = "Revamp of the VrooomGG website and included Damage Calculator";
+    $proj2desc = "Classic maplestory 2007 reverse engineered"
+    ?>
+
+    <title><?=$title?></title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=<?=$title?>>
+    <meta name="author" content=<?=$author?>>
+    <link rel="shortcut icon" href=<?=$fav?>>
+    
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    
+    <!-- FontAwesome JS-->
+	<script defer src="assets/fontawesome/js/all.min.js"></script>
+       
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/pillar-1.css">
+
+
+</head> 
+
+<body>
+    <article class="resume-wrapper text-center position-relative">
+	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
+		    <header class="resume-header pt-4 pt-md-0">
+			    <div class="row">
+				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+				        <img class="picture" src="assets/images/profile.jpg" alt="">
+				    </div><!--//col-->
+				    <div class="col">
+					    <div class="row p-4 justify-content-center justify-content-md-between">
+						    <div class="primary-info col-auto">
+							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase"><?=$author?>></h1>
+							    <div class="title mb-3"><?=$job_title?></div>
+							    <ul class="list-unstyled">
+								    <li class="mb-2"><a class="text-link" href="#"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i><?=$email?></a></li>
+								    <li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i>0123 456 78900</a></li>
+							    </ul>
+						    </div><!--//primary-info-->
+						    <div class="secondary-info col-auto mt-2">
+							    <ul class="resume-social list-unstyled">
+					                <li class="mb-3"><a class="text-link" href="<?="https://www.",$linkedin?>"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span><?=$linkedin?>></a></li>
+					                <li class="mb-3"><a class="text-link" href="<?="https://www.",$github?>"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span><?=$github?></a></li>
+							    </ul>
+						    </div><!--//secondary-info-->
+					    </div><!--//row-->
+					    
+				    </div><!--//col-->
+			    </div><!--//row-->
+		    </header>
+		    <div class="resume-body p-5">
+			    <section class="resume-section summary-section mb-5">
+				    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Summary</h2>
+				    <div class="resume-section-content">
+					    <p class="mb-0"><?=$description?></p>
+				    </div>
+			    </section><!--//summary-section-->
+			    <div class="row">
+				    <div class="col-lg-9">
+					    <section class="resume-section experience-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Work Experience</h2>
+						    <div class="resume-section-content">
+							    <div class="resume-timeline position-relative">
+								    <article class="resume-timeline-item position-relative pb-5">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1"><?=$job_title?></h3>
+										        <div class="resume-company-name ms-auto"><?=$workplace?></div>
+										    </div><!--//row-->
+										    <div class="resume-position-time"><?=$workdate?></div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    <p><?=$job_desc?></p>
+									    </div><!--//resume-timeline-item-desc-->
+
+								    </article><!--//resume-timeline-item-->
+
+							    
+							    
+							    
+							    
+							    
+						    </div>
+					    </section><!--//projects-section-->
+				    </div>
+				    <div class="col-lg-3">
+					    <section class="resume-section skills-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Skills &amp; Tools</h2>
+						    <div class="resume-section-content">
+						        <div class="resume-skill-item">
+							        <ul class="list-unstyled mb-4">
+								        <li class="mb-2">
+								            <div class="resume-skill-name">C</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 98%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">Reverse Engineering</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 94%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">Rust</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 96%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        
+								        <li class="mb-2">
+								            <div class="resume-skill-name">Laravel</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 92%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">Java</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 96%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+							        </ul>
+						        </div><!--//resume-skill-item-->
+						        <div class="resume-skill-item">
+						            <h4 class="resume-skills-cat font-weight-bold">Others</h4>
+						            <ul class="list-inline">
+							            <li class="list-inline-item"><span class="badge badge-light">Ghidra</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">IDA Pro</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Xdbg64</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Unit Testing</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">NeoVIM</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Lua</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">psql</span></li>
+						            </ul>
+						        </div><!--//resume-skill-item-->
+						    </div><!--resume-section-content-->
+					    </section><!--//skills-section-->
+					    <section class="resume-section education-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Education</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled">
+								    <li class="mb-2">
+								        <div class="resume-degree font-weight-bold"><?=$degree?></div>
+								        <div class="resume-degree-org"><?=$college?></div>
+								        <div class="resume-degree-time"><?=$workdate?></div>
+								    </li>
+							    </ul>
+						    </div>
+					    </section><!--//education-section-->
+					    <section class="resume-section language-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Languages</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled resume-lang-list">
+								    <li class="mb-2"><span class="resume-lang-name font-weight-bold">English</span> <small class="text-muted font-weight-normal">(Native)</small></li>
+								    <li class="mb-2 align-middle"><span class="resume-lang-name font-weight-bold">Japanese</span> <small class="text-muted font-weight-normal">(Family)</small></li>
+							    </ul>
+						    </div>
+					    </section><!--//language-section-->
+					    <section class="resume-section interests-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Interests</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled">
+								    <li class="mb-1"><?=$hobby1?></li>
+								    <li class="mb-1"><?=$hobby2?></li>
+								    <li class="mb-1"><?=$hobby3?></li>
+							    </ul>
+						    </div>
+					    </section><!--//interests-section-->
+					    
+				    </div>
+			    </div><!--//row-->
+				<section class="resume-section experience-section mb-5">
+					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Projects</h2>
+					<div class="row mt-4">
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image1.jpg" alt="Project 1" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title"><?=$proj1name?></h5>
+									<p class="card-text"><?=$proj1desc?></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image2.jpg" alt="Project 2" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title"><?=$proj2name?></h5>
+									<p class="card-text"><?=$proj2desc?></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section><!--//projects-section-->
+		    </div><!--//resume-body-->
+		    
+		    
+	    </div>
+    </article> 
+
+    
+    <footer class="footer text-center pt-2 pb-5">
+	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i><?=$author?></small>
+    </footer>
+
+    
+
+</body>
+</html>
+
